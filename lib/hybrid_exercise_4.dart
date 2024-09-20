07c926e571cea493f108f5bc6e3f29cb941c22df
Mark Corbin <markpc1608@gmail.com>
2024-09-20 00:23:58 -0400

    fetch data

--- lib/hybrid_exercise_4.dart
+++ lib/hybrid_exercise_4.dart
@@ -4,12 +4,16 @@ import 'dart:convert' as convert;
 
 void main() async {
   try {
-    final url = Uri.parse('https://random-data-api.com/api/users/random_user');
+    final url = Uri.parse('https://random-data-api.com/api/v2/users?size=5');
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
-      var data = convert.jsonDecode(response.body);
-      print(data);
+      var dataJson = convert.jsonDecode(response.body);
+
+      for (var data in dataJson) {
+        print(
+            "firstname: ${data['first_name']}, lastname: ${data['last_name']}, uid: ${data['uid']}");
+      }
     } else {
       print('Request failed with status: ${response.statusCode}.');
     }
